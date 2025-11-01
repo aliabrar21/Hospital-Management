b6cee11d6eb78fe758aa037327187f48a608ba83
Abrar <sonuabrar1999@gmail.com>
2025-11-02 00:47:00 +0530

    first commit

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -79,56 +79,60 @@ const Doctors = () => {
   };
 
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-16">
-      <div className=" flex flex-col items-center lg:flex-row justify-between mb-10 lg:mb-0">
-        <div>
-          <h1 className=" text-4xl font-semibold text-center lg:text-start">
-            Our Doctors
-          </h1>
-          <p className=" mt-2 text-center lg:text-start">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
-            quidem.
-          </p>
-        </div>
-        <div className="flex gap-5 mt-4 lg:mt-0">
-          <button
-            className=" bg-[#d5f2ec] text-backgroundColor px-4 py-2 rounded-lg active:bg-[#ade9dc]"
-            onClick={() => slider.current.slickPrev()}
-          >
-            <FaArrowLeft size={25} />
-          </button>
-          <button
-            className=" bg-[#d5f2ec] text-backgroundColor px-4 py-2 rounded-lg active:bg-[#ade9dc]"
-            onClick={() => slider.current.slickNext()}
-          >
-            <FaArrowRight size={25} />
-          </button>
-        </div>
-      </div>
-      <div className=" mt-5">
-        <Slider ref={slider} {...settings}>
-          {data.map((e, index) => (
-            <div
-              className="h-[350px] text-black rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] mb-2 cursor-pointer"
-              key={index}
-            >
-              <div>
-                <img
-                  src={e.img}
-                  alt="img"
-                  className=" h-56 rounded-t-xl w-full"
-                />
-              </div>
+      <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-16">
+        <div className="flex flex-col items-center lg:flex-row justify-between mb-10 lg:mb-0">
+          <div>
+            <h1 className="text-4xl font-semibold text-center lg:text-start">
+              Our Doctors
+            </h1>
+            <p className="mt-2 text-center lg:text-start">
+              Meet our team of experienced and compassionate doctors who are dedicated to
+              providing expert medical care across various specialties. Each of our physicians
+              brings years of experience, advanced knowledge, and a deep commitment to your
+              health and well-being.
+            </p>
+          </div>
 
-              <div className=" flex flex-col justify-center items-center">
-                <h1 className=" font-semibold text-xl pt-4">{e.name}</h1>
-                <h3 className=" pt-2">{e.specialties}</h3>
-              </div>
-            </div>
-          ))}
-        </Slider>
+          <div className="flex gap-5 mt-4 lg:mt-0">
+            <button
+                className="bg-[#d5f2ec] text-backgroundColor px-4 py-2 rounded-lg active:bg-[#ade9dc]"
+                onClick={() => slider.current.slickPrev()}
+            >
+              <FaArrowLeft size={25} />
+            </button>
+            <button
+                className="bg-[#d5f2ec] text-backgroundColor px-4 py-2 rounded-lg active:bg-[#ade9dc]"
+                onClick={() => slider.current.slickNext()}
+            >
+              <FaArrowRight size={25} />
+            </button>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <Slider ref={slider} {...settings}>
+            {data.map((e, index) => (
+                <div
+                    className="h-[350px] text-black rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] mb-2 cursor-pointer"
+                    key={index}
+                >
+                  <div>
+                    <img
+                        src={e.img}
+                        alt="img"
+                        className="h-56 rounded-t-xl w-full object-cover"
+                    />
+                  </div>
+
+                  <div className="flex flex-col justify-center items-center">
+                    <h1 className="font-semibold text-xl pt-4">{e.name}</h1>
+                    <h3 className="pt-2">{e.specialties}</h3>
+                  </div>
+                </div>
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div>
   );
 };
 
